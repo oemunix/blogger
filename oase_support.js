f7d2cb94884785c8a9cd347e6e17eda9e044b5e7
Oemar Unix <oemunix@facebook.com>
2018-03-08 07:17:06 +0700

    Create oase_support.js

---
+++ oase_support.js
@@ -0,0 +1,12 @@
+ window.intergramId = "213607835";
+  window.intergramCustomizations = {
+    titleClosed: 'Support & Bantuan',
+    titleOpen: 'Live Chat',
+    introMessage: 'Online 07:00 s.d 21:00 WIB | SMS/WA 085891165558',
+    autoResponse: 'pesan sudah diterima, ditunggu yah bos',
+    autoNoResponse: 'ditunggu bos',
+
+    // Can be any css supported color 'red', 'rgb(255,87,34)', etc
+    mainColor: "#26A1DD",
+    alwaysUseFloatingButton: false // use the mobile floating button also on large screens
+  };
