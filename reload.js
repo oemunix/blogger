ee1c852a9ef888f572c1a709887e7416369d57e6
Oemar Unix <oemunix@facebook.com>
2018-07-01 09:02:28 +0700

    Create reload.js

---
+++ reload.js
@@ -0,0 +1,12 @@
+ window.intergramId = "213607835";
+  window.intergramCustomizations = {
+    titleClosed: 'Live Chat',
+    titleOpen: 'Chat Sekarang',
+    introMessage: 'Hallo Bos! selamat datang di Reload.id, Ada yang bisa saya bantu ?',
+    autoResponse: 'Ditunggu bos... Fast Respon : WA 085891165558 | Telegram @maspulsa',
+    autoNoResponse: 'Mimin sedang Offline bos, WA 085891165558 | Telegram @maspulsa',
+
+    // Can be any css supported color 'red', 'rgb(255,87,34)', etc
+    mainColor: "#26A1DD",
+    alwaysUseFloatingButton: false // use the mobile floating button also on large screens
+  };
