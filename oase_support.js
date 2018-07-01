f22d027190f4392d34da92422d3b14cd8c7abafb
Oemar Unix <oemunix@facebook.com>
2018-07-01 08:54:32 +0700

    Update oase_support.js

--- oase_support.js
+++ oase_support.js
@@ -2,9 +2,9 @@
   window.intergramCustomizations = {
     titleClosed: 'Support & Bantuan',
     titleOpen: 'Live Chat',
-    introMessage: 'Online 07:00 s.d 21:00 WIB | SMS/WA 085891165558',
-    autoResponse: 'pesan sudah diterima, ditunggu yah bos',
-    autoNoResponse: 'Mimin sedang Offline bos, SMS/WA 085891165558 | Telegram @maspulsa',
+    introMessage: 'Online 07:00 s.d 21:00 WIB | WA 085891165558 | TG @maspulsa',
+    autoResponse: 'Fast Respon : WA 085891165558 | Telegram @maspulsa',
+    autoNoResponse: 'Mimin sedang Offline bos, WA 085891165558 | Telegram @maspulsa',
 
     // Can be any css supported color 'red', 'rgb(255,87,34)', etc
     mainColor: "#26A1DD",
